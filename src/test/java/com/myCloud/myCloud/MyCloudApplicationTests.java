e26c6bfce9bc63b7dddbe41175fc2eb159ae1ae9
kor-kms <minsuk0704@naver.com>
2022-04-10 14:58:10 +0900

    Feat: first project

---
+++ src/test/java/com/myCloud/myCloud/MyCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myCloud.myCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCloudApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
